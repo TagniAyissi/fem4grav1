c509bdca2aad7092305704a7a454303a7f874baa
Tagni Ayissi Mballa <tagniayissimballa@gmail.com>
2026-06-15 15:01:41 +0200

    add conftest.py for pytest path resolution

--- fem4grav/__init__.py
+++ fem4grav/__init__.py
@@ -2,3 +2,35 @@
 # -*- coding: utf-8 -*-
 
 from .__version__ import __version__
+from .fem4grav import (
+    FemResult,
+    compute_regional,
+    extract_nodes,
+    load_xyz,
+    middle_value,
+    regular_grid,
+    run_fem,
+    save_grid,
+    save_table,
+    separate_grid,
+    shape_functions,
+    summary,
+    validate_grid,
+)
+
+__all__ = [
+    "__version__",
+    "FemResult",
+    "compute_regional",
+    "extract_nodes",
+    "load_xyz",
+    "middle_value",
+    "regular_grid",
+    "run_fem",
+    "save_grid",
+    "save_table",
+    "separate_grid",
+    "shape_functions",
+    "summary",
+    "validate_grid",
+]
